cf5d952a49e0f39ae9eeb8b7bbcd27a159381177
pvdvreede <paul@vdvreede.net>
2016-01-28 10:26:37 +1100

    Add params file option to merge with cli params args for create.

--- lib/cfn-toolkit/stack_running_parser.rb
+++ lib/cfn-toolkit/stack_running_parser.rb
@@ -1,4 +1,5 @@
 require 'slop'
+require 'json'
 
 module Slop
   class KeyValuesOption < ArrayOption
@@ -9,12 +10,26 @@ module Slop
       end
     end
   end
+
+  class ParamsFileOption < ArrayOption
+    def call(value)
+      self.value = JSON.parse(File.read(value))
+    end
+
+    def finish(opts)
+      self.value = opts[:paramsfile].map do |p|
+        { key: p["ParameterKey"], value: p["ParameterValue"] }
+      end
+    end
+  end
 end
 
 def parse_args(args = ARGV, input = ARGF)
   opts = opts_parser.parse(args)
+  opts[:params] += opts[:paramsfile]
   args.replace opts.arguments
   opts_h = opts.to_h
+  opts_h.delete(:paramsfile)
   opts_h[:stack_name] = args.pop
   opts_h[:template] = input.read
   opts_h
@@ -22,6 +37,7 @@ end
 
 def opts_parser
   Slop::Options.new do |o|
+    o.params_file '--paramsfile', 'A json cloudformation parameters file'
     o.key_values '--params', 'key=value pairs for the cfn params'
     o.key_values '--tags', 'key=value pairs for the cfn tags'
   end
--- spec/stack_running_parser_spec.rb
+++ spec/stack_running_parser_spec.rb
@@ -1,24 +1,31 @@
 require_relative '../lib/cfn-toolkit/stack_running_parser'
 
 describe 'Parser for stack create or update cli options' do
-  let(:options) { opts_parser }
+  let(:json_params) { "[{\"ParameterKey\": \"k\", \"ParameterValue\": \"v\"}]" }
+  before do
+    expect(File).to receive(:read).with('/my/file').and_return(json_params)
+  end
 
   describe '#opts_parser' do
+    let(:options) { opts_parser }
     it 'parses the key value of params and tags' do
-      expect(options.parse(%w[--params k=v,a=b --tags c=d]).to_h).to eq({
+      expect(options.parse(%w[--params k=v,a=b --tags c=d --paramsfile /my/file]).to_h).to eq({
         tags: [
           { key: 'c', value: 'd'},
         ],
         params: [
           { key: 'k', value: 'v'},
           { key: 'a', value: 'b'}
+        ],
+        paramsfile: [
+          { key: 'k', value: 'v'}
         ]
       })
     end
   end
 
   describe '#parse_args' do
-    let(:args)  { %w[mystack --tags a=b,c=d --params e=f,h=j] }
+    let(:args)  { %w[mystack --tags a=b,c=d --params e=f,h=j --paramsfile /my/file] }
     let(:input) { double('stdin', read: "I am a template")}
 
     it 'parses flags, positional and stdin' do
@@ -30,7 +37,8 @@ describe 'Parser for stack create or update cli options' do
         ],
         params: [
           { key: 'e', value: 'f'},
-          { key: 'h', value: 'j'}
+          { key: 'h', value: 'j'},
+          { key: 'k', value: 'v'}
         ],
         template: "I am a template"
       })
