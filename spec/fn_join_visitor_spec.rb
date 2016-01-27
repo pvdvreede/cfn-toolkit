962c2fad4a6c69e1e14375f3eec5271bac330009
pvdvreede <paul@vdvreede.net>
2016-01-27 13:45:44 +1100

    Add rspec with initial tests.

---
+++ spec/fn_join_visitor_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../lib/cfn-toolkit/fn_join_visitor'
+
+describe FnJoinVisitor do
+  subject do
+    FnJoinVisitor.new
+  end
+
+  it "returns an array as is" do
+    expect(subject.visit_Fn__Join([], {})).to eq []
+  end
+
+  it "parses a string into fn join array" do
+    expect(subject.visit_Fn__Join("a test with %{{Ref1}} and %{{Ref2}} and some more text", {})).to eq([
+          "",
+          [
+            "a test with ",
+            { "Ref" => "Ref1" },
+            " and ",
+            { "Ref" => "Ref2" },
+            " and some more text"
+          ]
+      ])
+  end
+end
